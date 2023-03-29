1e5b7658c85b5bacf3773efdf3a31bcdb0b167cc
ddebmath <iamddeb@gmail.com>
2023-03-29 13:19:21 -0700

    update with new speakers

---
+++ html/lunr-pretext-search-index.js
@@ -0,0 +1,139 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "home",
+  "level": "1",
+  "url": "home.html",
+  "type": "Section",
+  "number": "1",
+  "title": "Home",
+  "body": " Home  Welcome to my webpage. I am an Associate Professor of Mathematics at the Oregon Institute of Technology . Please navigate the links on the left to know more about the courses that I am teaching this term, and my other activities.  "
+},
+{
+  "id": "teaching",
+  "level": "1",
+  "url": "teaching.html",
+  "type": "Section",
+  "number": "2",
+  "title": "Teaching",
+  "body": " Teaching   Winter 2023  Classes that I'm teaching this term    MATH 252 - Integral Calculus    MATH 321 - Applied Differential Equations I    Check Canvas for course details and information        Oregon Institute of Technology (2013 - ) - Intermediate Algebra, College Algebra, Trigonometry, Differential Calculus, Integral Calculus, Sequences and Series, Vector Calculus I, II, Applied Differential Equations I, II, Linear Algebra I, II, Mathematical Structures, Introduction to Real Analysis, Number Theory, Discrete Mathematics, Applied PDE I, Numerical Methods I, II, III, Mathematical Statistics.     University of Toledo (2010 - 2013) - College Algebra, Trigonometry, Single Variable Calculus, Multivariable Calculus, Calculus for Life Sciences with Applications I, II, Calculus for Business with Applications II, Differential Equations, Honors Calculus II, Numerical Methods and Linear Algebra.     University of Kentucky (2004 - 2010) - College Algebra, Remedial College Algebra, Contemporary Mathematics, Calculus II, Multivariable Calculus, Business Calculus, Calculus for the Life Sciences, Ordinary Differential Equations.        "
+},
+{
+  "id": "research",
+  "level": "1",
+  "url": "research.html",
+  "type": "Section",
+  "number": "3",
+  "title": "Research",
+  "body": " Research  My research interests are in number theory. More specifically my work involves Diophantine equations, p-adic and algebraic number theory. I am also interested in the theory of Quadratic forms.  CV( .pdf )   Publications     The 21 Card Trick and its Generalization , arXiv.org:1809.04072      On the Diophantine Equation , arXiv.org:1709.01814      (with David Leep) The Poincaré series of a diagonal polynomial , Preprint, Abstract( .pdf)      (with David Leep) Powers in complete discrete valuation rings , Preprint, Abstract( .pdf)      Diagonal forms and the rationality of the Poincaré series , PhD. thesis, University of Kentucky, 2010.      Math Colloquium  I have been organizing the OIT Mathematics Colloquium since Fall 2018. We are always looking for speakers. If you are interested in giving a talk, then please contact me. Here are the list of former speakers. Fall 2018     Fall 2018      Dibyajyoti Deb, Dept. of Mathematics - The 21 Card Trick and Its Generalization, October 18, 2018.     David Hammond, Dept. of Mathematics - Fractal Geometry of Julia Sets, November 8, 2018.     Jesse Kinder, Dept. of Natural Sciences - Hartree-Fock Theory, November 29, 2018.     Winter 2019      Peter Overholser, Dept. of Mathematics - Artificial Neural Networks and Applications, January 24, 2019.     Pat Schaeffer, Dept. of Management - Factorial Designed Experiments Using Minitab Statistical Software for Laser Weld Process Robustness, February 11, 2019.     Rosanna Overholser, Dept. of Mathematics - Causation, not just correlation, February 14, 2019.     Yuehai Yang, Dept. of Natural Sciences - How \"Squishy\" is a Roll of Graphene, a.k.a., a Single-Walled Carbon Nanotube?, February 25, 2019     Randall Paul, Dept. of Mathematics - Mathematical Perspectives and Projective Geometry, March 14, 2019.     Spring 2019      Peter Overholser, Dept. of Mathematics - Curves, blocks, chains, and coins, April 18, 2019.     Matthew Sleep, Dept. of Civil Engineering - The use of probability and statistics in geotechnical engineering - A case study on the Whittier Narrows Dam, May 9, 2019.     Reid Anderson, Math Major - Mathematical Ears!, May 23, 2019.     David Brookes, Dept. of Physics, Cal State, Chico - Dabbling in the “dark arts:” A physicist does education statistics, June 6, 2019     Fall 2019      Gregg Waterman, Dept. of Mathematics - The Laplacian of a Graph: Is the Name Justified?, October 17, 2019.     Jim Fischer, Dept. of Mathematics - Introduction to Singularity Functions and How to Bend Beams with Your Mind, November 7, 2019.     Eve Klopf, Dept. of EERE - Application of Numerical Methods for Solving Problems in Electromagnetics, November 21, 2019.     Winter 2020      Jesse Kinder, Dept. of Natural Sciences - Hypercubes and Hyperspheres: Geometry in More Than Three Dimensions, January 23, 2020.     Gregg Waterman, Dept. of Mathematics - Vectors to Function Spaces, February 13, 2020.     Gregg Waterman, Dept. of Mathematics - A Basic Introduction to Wavelets, February 20, 2020.     Peter Overholser, Dept. of Mathematics - Information on information and why you can never come out ahead, no matter how hard you try, March 12, 2020.     Spring 2020   Talks cancelled due to COVID. >  Fall 2020      Alexis Harris, Math Major - Observing the Sensitivity of Step Sizes in a Neural Network, November 10, 2020.     Curtis Michaels, Math Major - The Regularizing Effects of the Heat Equation, November 24, 2020.     Winter 2021      Joseph Reid, Dept. of Mathematics - An Introduction to Modeling Outbreak Investigations, January 27, 2021.     Yan Jiao, Univ. of Adelaide, Australia - Design Electrocatalysts for Clean Energy Conversions – A Theoretical Point of View, February 10, 2021.     Jesse Kinder, Dept. of Natural Sciences - Monte Carlo simulation of epidemics on complex networks, February 24, 2021.     Spring 2021      Robyn Wilde, Dept. of Natural Sciences - Positronium collisions with atoms and molecules, April 21, 2021.     Austin Moyer, Math Major - The 2014 Ebola Outbreak, May 19, 2021.     Randall Paul, Dept. of Mathematics - Conics Like You’ve Never Seen Them, June 2, 2021.     Fall 2021      Randall Paul, Dept. of Mathematics - Harmonic Sets in Music and Art, October 21, 2021.     David Hammond, Dept. of Mathematics - The discrete Laplacian deconvolution method for differential interference contrast microscopy, November 4, 2021.     Dibyajyoti Deb, Dept. of Mathematics - The Geometry of Numbers, December 2, 2021.     Winter 2022      Rosanna Overholser, Dept. of Mathematics - A Tale of Three Frameworks for Estimating Uncertainty, January 20, 2022.     Peter Overholser, Dept. of Mathematics - Views From a Train , February 10, 2022.     David Johnston, Dept. of Natural Sciences - Fusion Energy, Ray Tracing, and the Normal Form, March 3, 2022.     Spring 2022      Randall Paul, Dept. of Mathematics - Dots and Boxes and Donuts, April 28, 2022.     Jesse Kinder (Dept. of Natural Sciences), Kim Kinder (M.D.) - The Most Beautiful Instrument, May 19, 2022.     Lukasz Zbroszczyk & Peter Bigica, Dept. of Mathematics - Using Machine Learning to Forecast Outcomes of Baseball At-Bats, June 2, 2022.     Fall 2022      Jesse Kinder, Dept. of Natural Sciences - Quantum Math, October 19, 2022.     Jason Smith, Dept. of Mathematics - Iteratively Counting Edges to Sum Squares and Cubes, November 9, 2022.     Su Jin Lee, Dept. of Geomatics - Introduction to GIS, December 7, 2022.     Winter 2023      Kenneth Davis, Dept. of Mathematics - Fair Division and the Problem of Redistricting, February 8, 2023.     Jesse Kinder (Dept. of Natural Sciences), Kim Kinder (M.D.) - What is a Quantum Computer?, February 22, 2023.     Tiernan Fogarty, Dept. of Mathematics - Modeling the hanging beam in a first term differential equations course, March 15, 2023.          "
+},
+{
+  "id": "p-13",
+  "level": "2",
+  "url": "research.html#p-13",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "The 21 Card Trick and its Generalization "
+},
+{
+  "id": "p-14",
+  "level": "2",
+  "url": "research.html#p-14",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "On the Diophantine Equation "
+},
+{
+  "id": "p-15",
+  "level": "2",
+  "url": "research.html#p-15",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "(with David Leep) The Poincaré series of a diagonal polynomial "
+},
+{
+  "id": "p-16",
+  "level": "2",
+  "url": "research.html#p-16",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "(with David Leep) Powers in complete discrete valuation rings "
+},
+{
+  "id": "p-17",
+  "level": "2",
+  "url": "research.html#p-17",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Diagonal forms and the rationality of the Poincaré series "
+},
+{
+  "id": "p-18",
+  "level": "2",
+  "url": "research.html#p-18",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "Fall 2018",
+  "body": "Fall 2018 Winter 2019 Spring 2019 Fall 2019 Winter 2020 Spring 2020 Fall 2020 Winter 2021 Spring 2021 Fall 2021 Winter 2022 Spring 2022 Fall 2022 Winter 2023 "
+},
+{
+  "id": "misc",
+  "level": "1",
+  "url": "misc.html",
+  "type": "Section",
+  "number": "4",
+  "title": "Miscellaneous",
+  "body": " Miscellaneous   About Me  I am an Associate Professor of Mathematics at the Oregon Institute of Technology . Before coming here, I was a Visiting Assistant Professor in the Department of Mathematics at the University of Toledo , OH from 2010 to 2013.  I completed my Ph.D. in Mathematics from the University of Kentucky in August 2010, under the supervision of Prof. David Leep . My research interests are in Number Theory and Quadratic Forms. My Ph.D. thesis was titled Diagonal Forms and Rationality of the Poincaré Series , which, loosely speaking, is the study of the number of solutions of a polynomial modulo powers of prime and the power series that it generates.  I did my undergraduate education -- B.S. (Hons.) in Mathematics from the Chennai Mathematical Institute , India in 2004.    Interests        Hiking      Crater Lake from Mt. Scott        Playing and making video games using the GameMaker Studio engine.    Watching Sports - NFL, Univ. of Kentucky Basketball, Cricket.    Playing Boardgames (If I can find time and people).     "
+},
+{
+  "id": "p-77",
+  "level": "2",
+  "url": "misc.html#p-77",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Diagonal Forms and Rationality of the Poincaré Series "
+},
+{
+  "id": "contact",
+  "level": "1",
+  "url": "contact.html",
+  "type": "Section",
+  "number": "5",
+  "title": "Contact",
+  "body": " Contact   Schedule for Winter 2023    Winter 2023 Schedule    Office - OW 115    Email - dibyajyoti.deb[at]oit[dot]edu    "
+},
+{
+  "id": "p-85",
+  "level": "2",
+  "url": "contact.html#p-85",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Office - OW 115 "
+},
+{
+  "id": "p-86",
+  "level": "2",
+  "url": "contact.html#p-86",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Email - dibyajyoti.deb[at]oit[dot]edu "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
